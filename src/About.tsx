10cb302f30b57f6e1850304fcab123753ed18bf3
hamza-hussain-10 <hamzafiverrr02@gmail.com>
2024-11-20 00:59:01 +0530

    about page css

--- src/About.tsx
+++ src/About.tsx
@@ -11,7 +11,7 @@ export default function About(){
           </div>
           <div className="flex px-4 sm:px-9 py-9 grow flex-col">
             <h2 className="text-2xl sm:text-3xl text-gray-700 font-medium  text-center">About Page</h2>
-            <p className="py-4 text-gray-700  text-center sm:text-lg">This is an assignment given from the Aadi Foundation with Microsot Workshop Session.</p>
+            <p className="py-4 text-gray-700  text-center sm:text-lg">This is an assignment given from the Aadi Foundation with Microsoft Workshop Session.</p>
           </div>
         </div>
        </>
